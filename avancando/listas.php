35dec7c510e84fcc9afa005aa41bbaf08aa85ce4
Eduardo Teixeira <dunirvana@gmail.com>
2022-11-27 11:18:50 -0300

    Arrays

---
+++ avancando/listas.php
@@ -0,0 +1,7 @@
+<?php
+
+//ou $idadeList = array(20, 19, 22, 35, 40, 33);
+$idadeList = [20, 19, 22, 35, 40, 33];
+$primeiraIdade = $idadeList[0];
+
+echo "Primeira idade $primeiraIdade." . PHP_EOL;
---
+++ avancando/loop-lista.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$idadeList = [20, 19, 22, 35, 40, 33];
+
+for ($i = 0; $i < count($idadeList); $i++) {
+  echo $idadeList[$i] . PHP_EOL;
+}
